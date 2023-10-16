188b3c7ac1b061b0aa1f137ed697143285489cbc
SuciuAndrea <146415039+SuciuAndrea@users.noreply.github.com>
2023-10-16 17:06:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace TabelDeAdevar
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Bitwise AND Truth Table");
+            Console.WriteLine("");
+            Console.WriteLine("| A | B | A&B |");
+
+
+            for (int A = 0; A <= 1; A++) ;
+            {
+                for (int B = 0; B <= 1; B++) ;
+                {
+                    int result = A & B;
+                    Console.WriteLine($"| {A} | {B} | {result} |");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
